ab370e20f61adfdd10308f3f9a0a4817df25172a
dannybaby <dannyfresh@163.com>
2014-11-03 23:51:22 +0800

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,36 @@ package com.company;
  * Created by ydeng on 11/3/2014.
  */
 public class Main {
+<<<<<<< Updated upstream
     public static void main(String[] args){
         Minimum_Path test = new Minimum_Path();
         int[][] s = new int[][]{{1,1,5},{3,5,1},{1,2,3}};
         System.out.println(test.minPathSum(s));
+=======
+
+    public static void main(String[] args) {
+        /*SingleList<String> testlist = new SingleList<String>();
+        testlist.add("a");
+        testlist.add("b");
+        testlist.add("c");
+        testlist.add("d");
+        ListNode<String> p = testlist.head;
+        while (p.next != null){
+            p = p.next;
+        }
+        p.next = testlist.head.next.next;
+        Linked_List_Cycle<String> res = new Linked_List_Cycle<String>();
+        System.out.println(res.FindCycleStartPoint(testlist.head));
+        System.out.println(res.CycleLength(testlist.head));
+        N_Queens test = new N_Queens();
+        List<String[]> res = test.solveNQueens2(100);
+        for (String[] re : res) {
+            System.out.println(Arrays.toString(re));*/
+        
+        Permutations test = new Permutations();
+        int[] num = new int[]{0,1,3};
+        System.out.println(test.permute(num));
+        
+>>>>>>> Stashed changes
     }
 }
